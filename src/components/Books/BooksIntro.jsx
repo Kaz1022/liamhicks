7cc765122d2be87af02eb095a4fb4a1ef3a3f05e
Kazuko Baynton <kazukobaynton@gmail.com>
2022-09-01 15:03:06 -0700

    add responsive design on books intro

--- src/components/Books/BooksIntro.jsx
+++ src/components/Books/BooksIntro.jsx
@@ -14,13 +14,13 @@ function BooksIntro() {
     <div className="bintro-wrapper" id="booksintro">
       <div className="bi-top">
         <h1>Liam Hicks Book Series</h1>
-         {/* animation */}
+        {/* animation */}
         <div className="bi-background">
-          <motion.img 
-           initial={{top: '5rem', right:'-18rem'}}
-           whileInView={{top: '5rem', right:'-10rem'}}
-           transition={transition}
-           src={Bluejay} alt="" />
+            <motion.img 
+            initial={{top: '5rem', right:'-18rem'}}
+            whileInView={{top: '5rem', right:'-10rem'}}
+            transition={transition}
+            src={Bluejay} alt="" />
         </div>
       </div>
       <div className="bi-bottom">
