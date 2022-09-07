d9de7a736ee4f3c96f341ff1c7fb990ceadd0fed
Kazuko Baynton <kazukobaynton@gmail.com>
2022-09-07 13:10:30 -0700

    change copyright font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faSquareFacebook, faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faCopyright } from '@fortawesome/free-regular-svg-icons'
 import '../Sass/Footer.scss';
 
 function Footer() {
   return (
     <div className="footer">
       <div className="f-content">
-        <span>&copy; Brian Baynton</span>
+        <span><FontAwesomeIcon icon={faCopyright} size="s" /> Brian Baynton</span>
         <div className="f-icons">
           <a href="https://twitter.com/brian_baynton13" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faSquareTwitter} size="xl" />
